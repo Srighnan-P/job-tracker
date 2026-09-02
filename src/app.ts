ba3d64138805c5e7d2906871e053415b9d68f76d
Srighnan <srighnanpitchika@gmail.com>
2026-09-02 23:40:27 +0530

    add: implemented the cors in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes.js"
 import applicationRoutes from "./routes/application.routes.js"
 import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/application", applicationRoutes);
